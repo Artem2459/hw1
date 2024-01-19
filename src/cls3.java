40d570f2347a8cafb99e69f67e63467b9d0a4ffd
Artem Chornenkyi <temache888@gmail.com>
2024-01-19 19:47:49 +0200

    first commit

--- src/cls3.java
+++ src/cls3.java
@@ -1,2 +1,8 @@
 public class cls3 {
+    public static void main(String[] args) {
+        String имя = "Артем";
+        int возраст = 16;
+        System.out.println("Моё имя: " + имя);
+        System.out.println("Мой возраст: " + возраст);
+    }
 }
