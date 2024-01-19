f2bb729575979826b0f4667d300208c30f6b53c0
Artem Chornenkyi <temache888@gmail.com>
2024-01-19 19:44:28 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String имя = "Артем";
+        int возраст = 16;
+        System.out.println("Моё имя: " + имя);
+        System.out.println("Мой возраст: " + возраст);
+    }
+}
---
+++ src/hw2.java
@@ -0,0 +1,109 @@
+////1.import java.util.Scanner;
+////
+////public class hw2 {
+////    public static void main(String[] args) {
+////        Scanner scanner = new Scanner(System.in);
+////        System.out.print("Введите значение переменной a: ");
+////        int a = scanner.nextInt();
+////
+////        if (a == 0) {
+////            System.out.println("Верно");
+////        } else {
+////            System.out.println("Неверно");
+////        }
+////    }
+////}
+//
+//2.import java.util.Scanner;
+//
+//public class hw2 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите значение переменной a: ");
+//        int a = scanner.nextInt();
+//
+//        if (a > 0) {
+//            System.out.println("Верно");
+//        } else {
+//            System.out.println("Неверно");
+//        }
+//    }
+//}
+//3.import java.util.Scanner;
+//
+//public class hw2 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите значение переменной a: ");
+//        int a = scanner.nextInt();
+//
+//        if (a < 0) {
+//            System.out.println("Верно");
+//        } else {
+//            System.out.println("Неверно");
+//        }
+//    }
+//}
+//
+//      4.  import java.util.Scanner;
+//
+//public class hw2 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите значение переменной a: ");
+//        int a = scanner.nextInt();
+//
+//        if (a >= 0) {
+//            System.out.println("Верно");
+//        } else {
+//            System.out.println("Неверно");
+//        }
+//    }
+//}
+//
+//       5. import java.util.Scanner;
+//
+//public class hw2 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите значение переменной a: ");
+//        int a = scanner.nextInt();
+//
+//        if (a <= 0) {
+//            System.out.println("Верно");
+//        } else {
+//            System.out.println("Неверно");
+//        }
+//    }
+//}
+//
+//6.import java.util.Scanner;
+//
+//public class hw2 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Введите значение переменной a: ");
+//        int a = scanner.nextInt();
+//
+//        if (a != 0) {
+//            System.out.println("Верно");
+//        } else {
+//            System.out.println("Неверно");
+//        }
+//    }
+//}
+import java.util.Scanner;
+
+public class hw2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите значение переменной a: ");
+        int a = scanner.nextInt();
+
+        if (a == 0) {
+            System.out.println("Верно");
+        } else {
+            System.out.println("Неверно");
+        }
+    }
+}
