c08aa83a0dd564777fe3f30f8b9d884f52468d2b
Artem2459 <130496996+Artem2459@users.noreply.github.com>
2024-04-16 17:27:15 +0200

    Add files via upload

---
+++ hw22.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class hw22 {
+
+    @Test
+    public void testИмя() {
+        String имя = "Артем";
+        assertEquals("Артем", имя);
+    }
+
+    @Test
+    public void testВозраст() {
+        int возраст = 16;
+        assertEquals(16, возраст);
+    }
+
+    @Test
+    public void testВывод() {
+        String имя = "Артем";
+        int возраст = 16;
+        String ожидаемыйВывод = "Моё имя: Артем\nМой возраст: 16\n";
+
+        // Подготовка вывода
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        Main.main(null);
+
+        String фактическийВывод = outputStream.toString();
+
+        assertEquals(ожидаемыйВывод, фактическийВывод);
+    }
+}
